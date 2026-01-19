2f1cb2f51a42bbe21ec451b95641ca8efade4d6b
Scarlett253 <yvonnarriola@gmail.com>
2026-01-18 20:54:56 -0500

    Something wrong in q3, debbuging......

--- works/task1/js/q3.js
+++ works/task1/js/q3.js
@@ -18,13 +18,19 @@ let r2Y = 150;
 
 //rectangle 3 
 const r3Size = 80;
+const r3Speed = 2;
 let r3X = 100;
-let r3Y = 220;
+// let r3Y = 220;
+
+
 
 function setup() {
     console.log("go")
     createCanvas(500, 500);
     drawRectangle();
+
+    // //reset rect 3 when it touches the bottom
+    // resetRect3();
 
 }
 
@@ -34,7 +40,10 @@ function draw() {
     //rectangles
     drawRectangle(r1X, r1Y, r1Size, r1Size, 206, 56, 156);
     drawRectangle(r2X, r2Y, r2Size, r2Size, 224, 176, 255);
-    drawRectangle(r3X, r3Y, r3Size, r3Size, 127, 0, 255);
+    drawRectangle(r3.x, r3.y, r3.size, r3.size, 127, 0, 255);
+
+    // //make rect 3 move
+    // moveRect3();
 
 }
 function drawRectangle(x, y, w, h, r, g, b) { //body
@@ -44,3 +53,28 @@ function drawRectangle(x, y, w, h, r, g, b) { //body
     rect(x, y, w, h);
     pop();
 }
+
+// //Make rect 3 move down
+// function moveRect3() {
+//     r3.y += r3.speed;
+//     //reset rect 3
+//     if (r3.y > height) {
+//         resetRect3();
+//     }
+// }
+
+//Update rect 1 position when mouse is clicked
+function mouseClicked() {
+    r1X = random(0, width);
+    r1Y = random(0, height);
+}
+
+//Update rect 2 position when key is pressed
+function keyPressed() {
+    if (key === ' ') {//spacebar 
+        r2X = random(0, width);
+        r2Y = random(0, height);
+    }
+}
+
+
